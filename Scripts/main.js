4fa992e7144974af2926b6d7360b099e9bc5cc56
Nevec02 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2023-11-06 21:20:00 +0100

    aaa

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,11 @@
 import { listProducts } from "./listproducts.js";
 import { insertProducts } from "./insertProducts.js";
 import { searchItem } from "./search.js";
+import { sounds } from "./sounds.js";
 
 
 document.addEventListener("DOMContentLoaded", (listProducts));
+document.addEventListener("DOMContentLoaded", (sounds));
 
 const searchInput = document.getElementById("searchItem");
 
